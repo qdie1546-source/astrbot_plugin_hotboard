b03645c586b6c84d99c664edb526a1ed8b9d5316
qdie1546-source <qdie1546@gmail.com>
2026-03-29 02:16:37 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ from astrbot.api.event import AstrMessageEvent, filter
 from astrbot.api import logger
 
 API_URL = "https://uapis.cn/api/v1/misc/hotboard"
+DEFAULT_API_KEY = "uapi-zrvbf3gaVhkhUfPrKrzOHpYA5ZU2ij3pz5kM3nNs"
 
-@register("astrbot_plugin_hotboard", "星落云", "热点榜单插件", "2.0.2")
+PLATFORMS = [
+"bilibili","acfun","weibo","zhihu","zhihu-daily","douyin","kuaishou",
+"douban-movie","douban-group","tieba","hupu","ngabbs","v2ex",
+"52pojie","hostloc","coolapk","baidu","thepaper","toutiao",
+"qq-news","sina","sina-news","netease-news","huxiu","ifanr",
+"sspai","ithome","ithome-xijiayi","juejin","jianshu","guokr",
+"36kr","51cto","csdn","nodeseek","hellogithub","lol","genshin",
+"honkai","starrail","netease-music","qq-music","weread",
+"weatheralarm","earthquake","history"
+]
+
+@register("astrbot_plugin_hotboard", "星落云", "热点榜单插件", "2.0.3")
 class HotBoardPlugin(Star):
 
     def __init__(self, context):
@@ -16,7 +28,7 @@ class HotBoardPlugin(Star):
 
     async def fetch(self, type_):
         headers = {
-            "Authorization": f"Bearer {self.config.get('api_key','')}"
+            "Authorization": f"Bearer {DEFAULT_API_KEY}"
         }
 
         async with httpx.AsyncClient(timeout=10) as client:
@@ -32,55 +44,43 @@ class HotBoardPlugin(Star):
 
                 nodes.append({
                     "type": "node",
-                    "data": {
-                        "name": "热点榜单",
-                        "uin": "10000",
-                        "content": f"【{t}】"
-                    }
+                    "data": {"name": "热点榜单", "uin": "10000", "content": f"【{t}】"}
                 })
 
-                for i in data["list"]:
+                for i in data.get("list", []):
                     nodes.append({
                         "type": "node",
-                        "data": {
-                            "name": "热点榜单",
-                            "uin": "10000",
-                            "content": f"{i['title']}（{i['hot_value']}）\n{i['url']}"
-                        }
+                        "data": {"name": "热点榜单", "uin": "10000", "content": f"{i['title']}（{i['hot_value']}）\n{i['url']}"}
                     })
 
             except Exception as e:
-                logger.error(e)
+                logger.error(f"{t} 获取失败: {e}")
 
         return nodes
 
     @filter.command("今日热点")
     async def hot(self, event: AstrMessageEvent, type_: str = None):
-
-        if type_:
-            types = [type_]
-        else:
-            types = self.config.get("default_types", ["weibo"])
-
+        types = [type_] if type_ else self.config.get("default_types", ["weibo"])
         if self.config.get("use_forward", True):
             msg = await self.build_forward(types)
             yield event.chain_result(msg)
         else:
-            yield event.plain_result(str(types))
+            text = ""
+            for t in types:
+                data = await self.fetch(t)
+                text += f"\n【{t}】\n"
+                for i in data.get("list", []):
+                    text += f"{i['title']}（{i['hot_value']}）\n{i['url']}\n\n"
+            yield event.plain_result(text)
 
     async def loop_push(self):
         await asyncio.sleep(10)
-
         while True:
             try:
                 if self.config.get("push_enabled", False):
-
                     types = self.config.get("default_types", ["weibo"])
                     msg = await self.build_forward(types)
-
                     await self.context.send_all(msg)
-
             except Exception as e:
-                logger.error(e)
-
+                logger.error(f"定时任务错误: {e}")
             await asyncio.sleep(self.config.get("interval", 1800))
